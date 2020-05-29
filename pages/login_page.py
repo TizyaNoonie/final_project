717c8798aa6082b3a7c91c3c6e0c203ae17de0b6
TizyaNoonie <tizya.noonie@gmail.com>
2020-05-29 13:21:16 +0300

    Added LoginPage object and tests for it. In addition, created 'locators.py' with locators for Page Objects.

---
+++ pages/login_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        current_url = self.browser.current_url
+        assert "login" in current_url, f"There is no 'login' in current url, which is '{current_url}'"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "There is no 'login form' on the page"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "There is no 'register form' on the page"
--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,10 @@
+import pytest
+
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
+@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
@@ -8,8 +12,18 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()
 
 
+@pytest.mark.skip
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_can_see_login_and_register_forms(browser):
+    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+    page.should_be_login_form()
+    page.should_be_register_form()
