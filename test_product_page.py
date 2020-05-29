fb07370be5365072407bbdf2891f7ac5b7b6d75e
TizyaNoonie <tizya.noonie@gmail.com>
2020-05-29 19:39:54 +0300

    Final fixes before the review.

--- test_product_page.py
+++ test_product_page.py
@@ -3,21 +3,29 @@ import pytest
 from .pages.basket_page import BasketPage
 from .pages.product_page import ProductPage
 
-product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-urls = [f"{product_base_link}/?promo=offer{n}" for n in range(10)]
 
-
-@pytest.mark.skip
-@pytest.mark.parametrize('link', urls)
+@pytest.mark.need_review
+@pytest.mark.parametrize('link',
+                         ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                          pytest.param(
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                              marks=pytest.mark.xfail),
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                          "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser, link):
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     page.should_add_to_basket()
 
 
 @pytest.mark.skip
-@pytest.mark.xfail
+@pytest.mark.xfail(reason="This test should fail")
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
@@ -35,7 +43,7 @@ def test_guest_cant_see_success_message(browser):
 
 
 @pytest.mark.skip
-@pytest.mark.xfail
+@pytest.mark.xfail(reason="This test should fail")
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
@@ -52,7 +60,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
-@pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -60,6 +68,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
     page = ProductPage(browser, link)
@@ -69,3 +78,11 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_not_be_products_in_basket()
     basket_page.should_be_empty_basket_text()
+
+
+@pytest.mark.need_review
+def test_user_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_add_to_basket()
